814c73023042335686aa1503ae0caf308fdd2de6
jonatoledojt-jpg <jonatoledo.jt@gmail.com>
2026-09-13 23:47:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "paz-taller-v1";
+const SHELL = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== "GET") return;
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copia = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copia));
+        return r;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
